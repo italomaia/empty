70e282c5a1caa9c552dad30c82734fe59b22807d
Italo Maia <italo.maia@live.com>
2015-08-18 13:04:04 -0300

    Fixed configuration name for datetime filter
    DATE_FORMAT => DATETIME_FORMAT

--- empty/filters.py
+++ empty/filters.py
@@ -9,5 +9,5 @@ def format_date(date):
 
 def format_datetime(datetime):
     config = current_app.config
-    datetime_format = getattr(config, 'DATE_FORMAT', '%Y/%m/%d %H:%M')
+    datetime_format = getattr(config, 'DATETIME_FORMAT', '%Y/%m/%d %H:%M')
     return datetime.strftime(datetime_format)
