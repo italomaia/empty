034d369c9b5c83ba41899c2f049ef86a34679c6e
Italo Maia <italo.maia@live.com>
2015-08-18 11:27:30 -0300

    Adding first tests
    Tests configuration loading

--- tests/test_app.py
+++ tests/test_app.py
@@ -19,9 +19,10 @@ def test_config_loading(test_configs):
 
 def test_config_from_environment_has_precedence(test_configs):
     import config, env_config
-    app = Empty('myapp')
+
     os.environ['APP_CONFIG'] = os.path.abspath('tests/test_configs/env_config.py')
-    Config = config.EmptyConfig(DEBUG=True)
+    app = Empty('myapp')
+    Config = config.EmptyConfig(DEBUG=not env_config.DEBUG)
     app.configure(Config)
-    ENV_DEBUG = env_config.DEBUG
-    assert app.config['DEBUG'] is ENV_DEBUG
+    assert app.config['DEBUG'] is env_config.DEBUG
+
