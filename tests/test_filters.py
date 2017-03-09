20d31023e2ea42cc8df6e0e8a3c45656a9499c7c
Italo Maia <italo.maia@gmail.com>
2017-03-08 23:15:25 -0300

    Fixed filters tests

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,7 +1,19 @@
 # coding:utf-8
 
-import pytest
-from empty import app_factory
+import pytest  # noqa: F401
+from empty import Empty, app_factory
+
+
+class EmptyWithDateFilter(Empty):
+    def configure_template_filters(self):
+        from empty.filters import format_date
+        self.add_template_filter(format_date, 'date')
+
+
+class EmptyWithDatetimeFilter(Empty):
+    def configure_template_filters(self):
+        from empty.filters import format_datetime
+        self.add_template_filter(format_datetime, 'datetime')
 
 
 def test_date_filter():
@@ -9,7 +21,7 @@ def test_date_filter():
     from flask import render_template_string
 
     today = date(year=2015, month=10, day=5)
-    app = app_factory(object, 'myapp')
+    app = app_factory(object, 'myapp', base_application=EmptyWithDateFilter)
 
     with app.app_context():
         text = render_template_string("{{today|date}}", today=today)
@@ -24,7 +36,7 @@ def test_date_filter_with_settings():
         DATE_FORMAT = '%d/%m/%Y'
 
     today = date(year=2015, month=10, day=5)
-    app = app_factory(Config, 'myapp')
+    app = app_factory(Config, 'myapp', base_application=EmptyWithDateFilter)
 
     with app.app_context():
         text = render_template_string("{{today|date}}", today=today)
@@ -36,7 +48,7 @@ def test_datetime_filter():
     from flask import render_template_string
 
     now = datetime(year=2015, month=10, day=5, hour=5, minute=8, second=10)
-    app = app_factory(object, 'myapp')
+    app = app_factory(object, 'myapp', base_application=EmptyWithDatetimeFilter)
 
     with app.app_context():
         text = render_template_string("{{now|datetime}}", now=now)
@@ -51,7 +63,7 @@ def test_datetime_filter_with_settings():
         DATETIME_FORMAT = '%d/%m/%Y %H:%M'
 
     now = datetime(year=2015, month=10, day=5, hour=5, minute=8, second=10)
-    app = app_factory(Config, 'myapp')
+    app = app_factory(Config, 'myapp', base_application=EmptyWithDatetimeFilter)
 
     with app.app_context():
         text = render_template_string("{{now|datetime}}", now=now)
